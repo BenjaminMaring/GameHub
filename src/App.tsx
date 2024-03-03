2ec5b63cb03c5ece6167f1ebea463835f9d91567
BenjaminMaring <benjammaring@gmail.com>
2024-03-03 10:07:13 -0800

    first push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import OhHell from './Pages/OhHell'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <OhHell />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/OhHell.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../CSS/ohHell.css'
+export default function ohHell() {
+
+
+    return (
+        <div className="oh-wrapper">
+            <nav className="oh-menu">
+
+            </nav>
+            <div className="oh-body">
+                <div className="oh-inner-body">
+                    <p>Oh Hell</p>
+                    <button></button>
+                </div>
+                <footer className="oh-footer">
+                    <p>Benjamin Maring 2024</p>
+                </footer>
+            </div>
+        </div>
+    )
+}
